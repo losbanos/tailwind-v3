da385ed3b3bf4318d32256f739e8f418ae9e9825
kimtaehoon <kimtaehoon@smilegate.com>
2024-03-06 11:16:46 +0900

    [flex] flex box complete

--- src/components/FlexBox.vue
+++ src/components/FlexBox.vue
@@ -3,9 +3,13 @@
 </script>
 
 <template>
-<section class="flex flex-wrap justify-center gap-2">
-  <div v-for="i in 15" :key="i">
-    <p class="p-4 text-center bg-gray-200 flex-shrink-0 w-14 h-14 text-white" :style="`height: ${i* 30 + 30}px`">{{ i }}</p>
+<section class="flex justify-center gap-2 flex-wrap items-end">
+  <div v-for="i in 15"
+       :key="i"
+       class="p-4 text-center bg-gray-500 flex-shrink-0 w-14 h-14 text-white"
+       :style="`height: ${i * 30 + 30}px`"
+  >
+    {{ i }}
   </div>
 </section>
 </template>
